4c1cc20ae1c2dcfa6cdd9939ef2b6f9583485027
Younes Talibi <younessetalibi11@gmail.com>
2025-05-14 14:13:01 +0100

    feat: add home page

--- src/Controller/Frontend/HomeController.php
+++ src/Controller/Frontend/HomeController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller\Frontend;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_frontend_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_frontend_home')]
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
+        // Get all products and categories
+        $products = $productRepository->findAll();
+        $categories = $categoryRepository->findAll();
         return $this->render('frontend/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products,
+            'categories' => $categories,
         ]);
     }
 }
