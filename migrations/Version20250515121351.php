d72bf872c111e694fed2dd7cbdeb4f2d3f2e7fbb
Younes Talibi <younessetalibi11@gmail.com>
2025-05-15 13:22:38 +0100

    fix: check quantity before create orderItem and handle delete product

---
+++ migrations/Version20250515121351.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250515121351 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE cart_item DROP FOREIGN KEY FK_F0FE25274584665A
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE cart_item CHANGE product_id product_id INT DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE cart_item ADD CONSTRAINT FK_F0FE25274584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE SET NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE cart_item DROP FOREIGN KEY FK_F0FE25274584665A
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE cart_item CHANGE product_id product_id INT NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE cart_item ADD CONSTRAINT FK_F0FE25274584665A FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+        SQL);
+    }
+}
